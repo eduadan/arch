2b75864209202fec6feb9dcbe392acbab80e7d6f
eduadan <cassettecd@yahoo.com.ar>
2017-05-29 16:41:56 -0300

    Add files via upload

---
+++ VMDeployAPSDSC.ps1
@@ -0,0 +1,27 @@
+$filename="VMDeployAPS_DSC.ps1"
+$localpath="d:\script\"
+New-Item -ItemType directory -Path $localpath
+
+start-Sleep -s 5
+
+$source = "http://10.246.82.132/VMDeployAPS_DSC.txt"
+$destination = $localpath+"VMDeployAPS_DSC.txt"
+ 
+#copy the file to LM
+Invoke-WebRequest $source -OutFile $destination -Verbose
+
+#rename thefile
+Rename-Item $destination $filename
+
+#set folder permisions
+
+$sharepath = $localpath
+$Acl = Get-ACL $SharePath
+$AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","read","ContainerInherit,Objectinherit","none","Allow")
+$Acl.AddAccessRule($AccessRule)
+Set-Acl $SharePath $Acl
+
+
+
+#run ps1
+Invoke-Expression "$localpath$filename"
